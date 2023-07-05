c0521d9f1b56865e29cd0c4fc6c697b5cc298243
MANAN <manananand11@gmail.com>
2023-07-05 18:40:27 +0530

    connected to db

---
+++ api/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
+const connectDB = require("./config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
